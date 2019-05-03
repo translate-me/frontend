fd08dc8f53a4fb70a21c6fd8bc0b64b762935fb1
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-05-03 18:39:24 -0300

    Add steps and change from card to div
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- frontend/src/Screens/HomepageAuthor.js
+++ frontend/src/Screens/HomepageAuthor.js
@@ -3,18 +3,19 @@ import NavBar from '../Components/NavBar';
 import { Card, Button } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faCartArrowDown } from '@fortawesome/free-solid-svg-icons'
+
 class HomepageAuthor extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            advantages:[
-                "Vantagem\n1",
-                "Vantagem\n2",
-                "Vantagem\n3",
-                "Vantagem\n4"
+        this.state = {
+            advantages: [
+                "Vantagem 1",
+                "Vantagem 2",
+                "Vantagem 3",
+                "Vantagem 4"
             ],
-            steps:[
+            steps: [
                 "Faça o upload do seu texto acadêmico aqui",
                 "Defina quais são os pontos que cada tradutor pode visualizar",
                 "Aguarde a tradução ser completa e revisada",
@@ -22,52 +23,63 @@ class HomepageAuthor extends Component {
             ]
         }
     }
-    render_card(name){
-        return(
-            <Card style={{ backgroundColor: "#E5E5E5" }}>
-                <i>{name}</i>
-            </Card>
-        );
+    render_step(item){
+        if (item !== this.state.steps[3]) {
+            return (
+                <FontAwesomeIcon style={{ margin: "15%", marginTop: "40px" }} icon={faArrowRight} />
+            );
+        }
     }
+
     render() {
         return (
             <div>
                 <NavBar/>
 
-                <Card style={{ backgroundColor:"#C4C4C4"}}>
+                <div style={{ backgroundColor: "#C4C4C4", 
+                    display: "flex",
+                    flexDirection: "column",
+                    alignText: "center",
+                    alignSelf: "center"
+                }}>
                     <p style={{
-                        marginTop: "5%",
+                        marginTop: "10%",
                         fontFamily: "Raleway",
-                        alignSelf:"center"
+                        textAlign: "center",
+                        padding: "5px",
+                        fontSize: "35px",
+                        marginLeft:"10%",
+                        marginRight: "10%"
                     }}>
-                    Traduza seus textos acadêmicos com qualidade, com uma tradução profissional! Agora, fácil assim!
+                        Traduza seus textos acadêmicos com qualidade, com uma tradução profissional! Agora, fácil assim!
                     </p>
-                    <Card style={{
-                        backgroundColor: "#F9FBFC",
-                        width: "40%",
+                    <div style={{
+                        backgroundColor: "#E5E5E5",
                         alignSelf: "center",
                         display: "flex",
-                        flexDirection: "row",
-                        padding:"2%",
-                        alignItems: "center",
-                        marginBottom: "5%"
+                        padding: "2%",
+                        marginBottom: "5%",
+                        borderRadius: "10px",
+                        width: "50%"
                     }}>
-                       {this.state.advantages.map((item) => {
-                           return(
-                           <Card style={{ backgroundColor: "#C4C4C4", margin: "10px", padding: "5px" }}>
-                                <i>{item}</i>
-                            </Card>)
-                       })}
-                    </Card>
+                        {this.state.advantages.map((item, i) => {
+                            return (
+                                <Card key={i} style={{
+                                    backgroundColor: "#C4C4C4", margin: "2%", padding: "2%", width: "100%"}}>
+                                    <Card.Title style={{ fontFamily: "Raleway", fontSize: "20px"}}>{item}</Card.Title>
+                                </Card>)
+                        })}
+                    </div>
                     <Button style={{
                         fontFamily: "Raleway",
                         alignSelf: "center",
                         backgroundColor: "#2B423E",
-                        marginBottom: "5%"
+                        marginBottom: "5%",
+                        borderColor: "#2B423E"
                         
-                    }}>Quero Traduzir Agora</Button>
-                </Card>
-                <Card style={{
+                    }}>Quero Traduzir Agora!</Button>
+                </div>
+                <div style={{
                     backgroundColor: "#C4C4C4",
                     marginTop: "2%",
                     alignSelf: "center",
@@ -78,26 +90,36 @@ class HomepageAuthor extends Component {
                 }}>
                     {this.state.steps.map((item, i)=>{
                         return(
-                            <div style={{display: "flex",flexDirection: "row",}}>
+                            <div key={i} style={{display: "flex",flexDirection: "collumn"}}>
                                 <Card style={{
                                     backgroundColor: "#E5E5E5",
                                     width: "100%",
-                                    height: "100px",
+                                    height: "100%",
                                     borderRadius: 0,
-                                    padding: "5px"
+                                    padding: "5px",
+                                    alignItems: "center"
                                 }}>
-                                    <i>{item}</i>
-                                    <h1>{i}</h1>
+                                    <Card.Title style={{ fontFamily: "Raleway", textAlign: "center",margin: "5px"}}>{item}</Card.Title>
+                                    <Card.Body>
+                                        <div style={{
+                                            backgroundColor: "#C4C4C4",
+                                            border: "1px",
+                                            borderRadius: "100%",
+                                            height: "30px",
+                                            width: "30px",
+                                            textAlign: "center"}}>
+                                            <p>{i+1}</p>
+                                        </div>
+                                    </Card.Body>
                                 </Card>
-                                {
-                                    item != this.state.steps[3]?
-                                        <FontAwesomeIcon style={{margin: "15%", marginTop:"40px"}} icon={faArrowRight} />
-                                    :<div/>
-                                }
+
+                                
+                                    {/* <FontAwesomeIcon style={{margin: "15%", marginTop:"40px"}} icon={faArrowRight} /> */}
+                                    
                             </div>
                         )
                     })}
-                </Card>
+                </div>                
             </div>
         );
     }
