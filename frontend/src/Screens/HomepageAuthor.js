7d3b875f014601cabd099a247e33058d0e716ab7
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-05-03 09:59:24 -0300

    Start Author homepage cards
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- frontend/src/Screens/HomepageAuthor.js
+++ frontend/src/Screens/HomepageAuthor.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import NavBar from '../Components/NavBar';
+import { Card } from 'react-bootstrap';
 
 class HomepageAuthor extends Component {
     render() {
         return (
-            <NavBar/>
+            <div>
+                <NavBar/>
+                <Card style={{ backgroundColor:"#E5E5E5"}}>
+                    <p style={{
+                        marginTop: "5%",
+                        fontFamily: "Raleway",
+                        alignSelf:"center"
+                    }}>
+                    Traduza seus textos acadêmicos com qualidade, com uma tradução profissional! Agora, fácil assim!
+                    </p>
+                    <Card style={{ backgroundColor: "#F9FBFC", width: "50%", alignSelf: "center"}}>
+                    <p>oie</p>
+                    </Card>
+                </Card>
+            </div>
         );
     }
 }
