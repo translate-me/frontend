bbb2a31de8797854e7f791b4882ef7a56ecaefb2
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-05-10 09:23:37 -0300

    Fix homepage codeclimate
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- frontend/src/Screens/HomepageAuthor.js
+++ frontend/src/Screens/HomepageAuthor.js
@@ -25,110 +25,37 @@ class HomepageAuthor extends Component {
 
     render() {
         return (
-            <div style={{
-                backgroundColor: white,
-                display: "flex",
-                flexDirection: "column",
-                alignText: "center",
-                alignSelf: "center"
-            }}>
+            <div style={styles.screen}>
                 <NavBar />
-                    <p style={{
-                        marginTop: "10%",
-                        fontFamily: "Raleway",
-                        textAlign: "center",
-                        padding: "5px",
-                        fontSize: "35px",
-                        marginLeft: "10%",
-                        marginRight: "10%"
-                    }}>
+                    <p style={styles.title}>
                         Traduza seus textos acadêmicos com qualidade, com uma tradução profissional! Agora, fácil assim!
                     </p>
-                    <div style={{
-                        borderColor: green,
-                        borderStyle: "solid",
-                        alignSelf: "center",
-                        display: "flex",
-                        padding: "2%",
-                        marginBottom: "5%",
-                        borderRadius: "10px",
-                        width: "50%"
-                    }}>
+                    <div style={styles.advantages_square}>
                         {this.state.advantages.map((item, i) => {
                             return (
-                                <Card key={i} style={{
-                                    backgroundColor: green,
-                                    margin: "2%",
-                                    padding: "2%",
-                                    width: "100%"
-                                }}>
-                                    <Card.Title style={{
-                                        fontFamily: "Raleway",
-                                        fontSize: "20px",
-                                        color: white,
-                                        justifyContent: "center",
-                                    }}>{item}</Card.Title>
+                                <Card key={i} style={styles.advantages_card}>
+                                    <Card.Title style={styles.advantages_title}>
+                                        {item}
+                                    </Card.Title>
                                 </Card>)
                         })}
                     </div>
-                    <Button style={{
-                        fontFamily: "Raleway",
-                        fontSize: "20px",
-                        alignSelf: "center",
-                        backgroundColor: "#2B423E",
-                        marginBottom: "5%",
-                        width: "30%",
-                        borderColor: "#2B423E"
+                    <Button style={styles.button}>Quero Traduzir Agora!</Button>
 
-                    }}>Quero Traduzir Agora!</Button>
-
-                <div style={{
-                    backgroundColor: lightgreen,
-                    marginTop: "2%",
-                    alignSelf: "center",
-                    display: "flex",
-                    flexDirection: "row",
-                    padding: "2%",
-                    alignItems: "center",
-                }}>
+                <div style={styles.steps_div}>
                     {this.state.steps.map((item, i) => {
                         return (
                             <div key={i} style={{ display: "flex", flexDirection: "collumn" }}>
-                                <Card style={{
-                                    borderColor: green,
-                                    borderStyle: "solid",
-                                    backgroundColor: white,
-                                    width: "100%",
-                                    height: "100%",
-                                    borderRadius: 0,
-                                    padding: "5px",
-                                    alignItems: "center",
-                                    margin: "5px"
-                                }}>
-                                    <Card.Title style={{
-                                        fontFamily: "Raleway",
-                                        textAlign: "center",
-                                        margin: "5px"
-                                    }}>{item}
+                                <Card style={styles.steps_card}>
+                                    <Card.Title style={styles.steps_title}>
+                                        {item}
                                     </Card.Title>
                                     <Card.Body>
-                                        <div style={{
-                                            border: "1px",
-                                            borderRadius: "100%",
-                                            borderStyle: "solid",
-                                            height: "30px",
-                                            width: "30px",
-                                            textAlign: "center",
-                                            backgroundColor: lightgreen
-                                        }}>
+                                        <div style={styles.number}>
                                             <p style={{color: white}}>{i + 1}</p>
                                         </div>
                                     </Card.Body>
                                 </Card>
-
-
-                                {/* <FontAwesomeIcon style={{margin: "15%", marginTop:"40px"}} icon={faArrowRight} /> */}
-
                             </div>
                         )
                     })}
@@ -138,6 +65,92 @@ class HomepageAuthor extends Component {
             
         );
     }
+}
+
+
+const styles = {
+    screen: {
+        backgroundColor: white,
+        display: "flex",
+        flexDirection: "column",
+        alignText: "center",
+        alignSelf: "center"
+    },
+    title: {
+        marginTop: "10%",
+        fontFamily: "Raleway",
+        textAlign: "center",
+        padding: "5px",
+        fontSize: "35px",
+        marginLeft: "10%",
+        marginRight: "10%"
+    },
+    advantages_square: {
+        borderColor: green,
+        borderStyle: "solid",
+        alignSelf: "center",
+        display: "flex",
+        padding: "2%",
+        marginBottom: "5%",
+        borderRadius: "10px",
+        width: "50%"
+    },
+    advantages_card: {
+        backgroundColor: green,
+        margin: "2%",
+        padding: "2%",
+        width: "100%"
+    },
+    advantages_title: {
+        fontFamily: "Raleway",
+        fontSize: "20px",
+        color: white,
+        justifyContent: "center"
+    },
+    button: {
+        fontFamily: "Raleway",
+        fontSize: "20px",
+        alignSelf: "center",
+        backgroundColor: "#2B423E",
+        marginBottom: "5%",
+        width: "30%",
+        borderColor: "#2B423E"
+    },
+    steps_div: {
+        backgroundColor: lightgreen,
+        marginTop: "2%",
+        alignSelf: "center",
+        display: "flex",
+        flexDirection: "row",
+        padding: "2%",
+        alignItems: "center",
+    },
+    steps_card: {
+        borderColor: green,
+        borderStyle: "solid",
+        backgroundColor: white,
+        width: "100%",
+        height: "100%",
+        borderRadius: 0,
+        padding: "5px",
+        alignItems: "center",
+        margin: "5px"
+    },
+    steps_title: {
+        fontFamily: "Raleway",
+        textAlign: "center",
+        margin: "5px"
+    },
+    number: {
+        border: "1px",
+        borderRadius: "100%",
+        borderStyle: "solid",
+        height: "30px",
+        width: "30px",
+        textAlign: "center",
+        backgroundColor: lightgreen
+    }
+
 }
 
 export default HomepageAuthor;
