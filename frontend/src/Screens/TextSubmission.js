d73e47b9c1c48c7f988f3c748be452584523efd1
Luiz Guilherme <do.guilherme@hotmail.com>
2019-05-27 20:34:10 -0300

    Add text uploader component and some styles
    Signed-off-by: Luiz Guilherme <do.guilherme@hotmail.com>
    Co-authored-by: Rômulo Vinicius <romulovinicius10@gmail.com>

--- frontend/src/Screens/TextSubmission.js
+++ frontend/src/Screens/TextSubmission.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Alert } from 'react-bootstrap';
 import { white, green, lightgreen } from '../colors'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Collapse, Form } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image'
+import TextareaAutosize from 'react-textarea-autosize';
 import Cloud from './cloud.png'
 import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
@@ -13,7 +14,8 @@ class TextSubmission extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            files: []
+            files: [],
+            upload_toggle: false
         }
     }
 
@@ -22,6 +24,7 @@ class TextSubmission extends Component {
     }
 
     render() {
+        const { upload_toggle } = this.state;
         return (
             <div style={styles.screen}>
                 <Container>
@@ -32,41 +35,72 @@ class TextSubmission extends Component {
                     </Row>
                     <Row>
                         <Col>
-                            <Container style={styles.upload_box}>
-                                <Row>
-                                    <Col>
-                                        <Image style={styles.cloud_image} src={Cloud} />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <p>
-                                            Arraste seu arquivo para cá <br/>ou
-                                        </p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <FilePond
-                                            ref={ref => (this.pond = ref)}
-                                            files={this.state.files}
-                                            allowMultiple={false}
-                                            maxFiles={1}
-                                            // server="/api"
-                                            oninit={() => this.handleInit()}
-                                            onupdatefiles={fileItems => {
-                                                // Set currently active file objects to this.state
-                                                this.setState({
-                                                    files: fileItems.map(fileItem => fileItem.file)
-                                                });
-                                                console.log(this.state.files)
-                                                // console.log(this.readTextFile(this.state.files[0]))
-                                            }}
-                                        />
-                                    </Col>
-                                </Row>
+                            <Collapse in={!this.state.upload_toggle}>
+                                <Container style={styles.upload_box}>
+                                    <Row>
+                                        <Col>
+                                            <Image style={styles.cloud_image} src={Cloud} />
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <p>
+                                                Arraste seu arquivo para cá <br/>ou
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <FilePond
+                                                ref={ref => (this.pond = ref)}
+                                                files={this.state.files}
+                                                allowMultiple={false}
+                                                maxFiles={1}
+                                                server="http://0.0.0.0:9000/filepond/process/"
+                                                oninit={() => this.handleInit()}
+                                                onupdatefiles={fileItems => {
+                                                    // Set currently active file objects to this.state
+                                                    this.setState({
+                                                        files: fileItems.map(fileItem => fileItem.file)
+                                                    });
+                                                    console.log(this.state.files)
+                                                    // console.log(this.readTextFile(this.state.files[0]))
+                                                }}
+                                            />
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Collapse>
 
-                            </Container>
+                                <Row>
+                                    <Col style={styles.center}>
+                                        <Alert.Link
+                                        onClick={() => this.setState({ upload_toggle: !upload_toggle })}
+                                        aria-controls="example-collapse-text"
+                                        aria-expanded={upload_toggle}
+                                        style={styles.link_toggle}
+                                        >
+                                            {upload_toggle ? 'Clique aqui para enviar um arquivo' : 'Clique aqui para digitar o texto'}
+                                        </Alert.Link>
+                                        <Collapse in={this.state.upload_toggle}>
+                                            <Form>
+                                                <Form.Group controlId="exampleForm">
+                                                    <TextareaAutosize style={styles.medium_width} />
+                                                    {/* <Form.Control as="textarea" rows="3" /> */}
+                                                </Form.Group>
+                                                <Row>
+                                                    <Col style={styles.center}>
+                                                        <Form.Label>Escreva seu texto ou copie e cole do seu arquivo</Form.Label>
+                                                    </Col>
+                                                </Row>
+                                                
+                                                <Button variant="primary" type="submit">
+                                                    Submit
+                                                </Button>
+                                            </Form>
+                                        </Collapse>
+                                    </Col>
+                                </Row>
                         </Col>
                     </Row>
                 </Container>
@@ -99,6 +133,15 @@ const styles = {
         textAlign: "center",
         border: "1px solid black",
         width: "50%"
+    },
+    link_toggle: {
+        color: "rgb(43, 66, 62)"
+    },
+    medium_width: {
+        width: "75%"
+    },
+    center: {
+        textAlign: "center"
     }
 
 }
