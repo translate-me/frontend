a5e173a8ddcb628d9c51bbae922abed6dacefd13
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2019-05-31 14:09:19 -0300

    Add sendFragment method to split the result

--- frontend/src/Screens/Breakpoints.js
+++ frontend/src/Screens/Breakpoints.js
@@ -3,42 +3,50 @@ import { render } from "react-dom";
 
 
 let breakpoints = [];
+let styles;
 
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
-};
-
-export default class Breakpoints extends React.Component {
+export class Breakpoints extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "hello"
+      value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia nibh ut arcu dignissim malesuada. Cras fermentum cursus augue vitae. ",
+      fragments: []
     };
   }
 
   onMouseUp = e => {
     const position = e.target.selectionStart;
     const text = e.target.value;
-    
+
     breakpoints.push(position)
 
     console.log(position);
     console.log('breakpoints: ', breakpoints)
 
     let array = Array.from(text)
-    array[position - 1] = array[position - 1] + '*'
-    console.log('array: ', array)
+    array[position - 1] = array[position - 1] + '🔴'
+    // console.log('array: ', array)
 
     let newArray = array.join('')
-    console.log('newArray: ', newArray)
+    // console.log('newArray: ', newArray)
 
     this.setState({
       value: newArray
     })
   };
 
-  
+  sendFragment = () => {
+    const completeText = this.state.value
+    const fragments = completeText.split('🔴')
+    
+    console.log(fragments)
+
+    this.setState({
+      fragments: fragments
+    })
+
+    console.log('state.frags', this.state.fragments)
+  }
 
   onChange = e => {
     this.setState({
@@ -48,12 +56,29 @@ export default class Breakpoints extends React.Component {
 
   render() {
     return (
-      <textarea
-        id="my-input"
-        onChange={this.onChange}
-        value={this.state.value}
-        onMouseUp={this.onMouseUp}
-      />
+      <div>
+        <textarea
+          style={styles.textArea}
+          id="my-input"
+          onChange={this.onChange}
+          value={this.state.value}
+          onMouseUp={this.onMouseUp}
+        />
+        <button id="send-fragment" onClick={this.sendFragment}>
+          Enviar Fragmentos
+        </button>
+      </div>
+
     );
   }
 }
+
+styles = {
+  textArea: {
+    width: '100%',
+  }
+};
+
+
+
+export default Breakpoints;
