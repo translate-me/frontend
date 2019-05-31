b39a6e09026d3ecc61cefd890c08508636ab0912
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2019-05-31 15:10:43 -0300

    Update usage of Bootstrap to remove CDN and use NPM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,12 @@ import Breakpoints from './Screens/Breakpoints';
 export const App = () => (
   <Router>
     <div>
-      <link
+      {/* <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
-      />
+      /> */}
       <link href="https://fonts.googleapis.com/css?family=Nixie+One|Raleway" rel="stylesheet" />
       <NavBar />
       <Route exact path="/" component={HomepageAuthor} />
