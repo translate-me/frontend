b08fad7d40bd05f1eca85a84f4642c06fba91f28
joao robson <joaorobsonmartins@gmail.com>
2019-06-03 21:20:53 -0300

    Change proxy config file location

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
 var proxy = require('http-proxy-middleware')
  
 module.exports = function(app){
-  app.use(proxy('/user', { target: 'http://localhost:8090/' }));
+  app.use(proxy('/user', { target: 'http://authentication:8090' }));
 }
