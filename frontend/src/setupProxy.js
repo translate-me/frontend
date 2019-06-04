279770e6499fb6f4bf943b34e7f53de468174f21
joao robson <joaorobsonmartins@gmail.com>
2019-06-04 13:53:40 -0300

    Fix conflicts

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
 var proxy = require('http-proxy-middleware')
  
 module.exports = function(app){
-  app.use(proxy('/user', { target: 'http://authentication:8090' }));
+  app.use(proxy('/user', { target: 'http://authentication_django:8090' }));
 }
